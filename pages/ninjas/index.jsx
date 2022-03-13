8825cc6aeecbea1f09efcd506672605dd1c0489c
pmadinga <phiwen2@gmail.com>
2022-03-13 20:19:54 +0200

    Fetch dummy data

--- pages/ninjas/index.jsx
+++ pages/ninjas/index.jsx
@@ -1,7 +1,18 @@
 import Head from 'next/head';
-import React from 'react';
+import styles from '../../styles/Ninjas.module.css'
+export const getStaticProps = async () => {
 
-const Ninjas = () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    return {
+        props: {ninjas: data}
+    }
+}
+
+const Ninjas = ({ninjas}) => {
+    // console.log(ninjas);
     return (
         <>  
             <Head>
@@ -10,6 +21,16 @@ const Ninjas = () => {
             </Head>
             <div>
                 <h1>All ninjas</h1>
+                {
+                    ninjas.map(ninja => (
+                        <div key={ninja.id}>
+                            <a className={styles.single}>
+                                <h3>{ninja.name}</h3>
+                            </a>
+
+                        </div>
+                    ))
+                }
             </div>
         </>
     );
