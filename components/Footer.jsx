5d17d5a82967426b0e3f1d7e7c00cda2ee2b0cd2
pmadinga <phiwen2@gmail.com>
2022-03-13 19:05:48 +0200

    Added styles

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div>
+    <footer>
         Copyright &copy; 2022
-    </div>
+    </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
+import style from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1 className={style.title}>Homepage</h1>
 
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, tempore. Odit tempora, quia modi perspiciatis deleniti consequuntur, molestiae sed omnis aut deserunt tempore doloremque dolore quasi delectus soluta aliquam quos.</p>
-      <Link href={'/ninjas'}><a>See Ninja listing</a></Link>
+      <p className={style.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, tempore. Odit tempora, quia modi perspiciatis deleniti consequuntur, molestiae sed omnis aut deserunt tempore doloremque dolore quasi delectus soluta aliquam quos.</p>
+      <p className={style.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, tempore. Odit tempora, quia modi perspiciatis deleniti consequuntur, molestiae sed omnis aut deserunt tempore doloremque dolore quasi delectus soluta aliquam quos.</p>
+      <Link href={'/ninjas'}>
+        <a className={style.btn}>See Ninja listing</a>
+      </Link>
     </div>
   )
 }
