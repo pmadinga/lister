5f64c6ef1f4729f33742b1d58bc72ae8d90a3545
pmadinga <phiwen2@gmail.com>
2022-03-13 17:16:17 +0200

    Project init

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+        Copyright &copy; 2022
+    </div>
+  )
+}
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dolor quidem consequuntur nesciunt, nam quis accusantium cupiditate, minus repudiandae autem vero? Velit, obcaecati optio quibusdam doloribus sed esse nisi fugit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dicta tempore nesciunt in ipsam recusandae dolores impedit sit voluptatibus, vitae perferendis dignissimos ex nostrum, consectetur voluptas at, magnam quam. Fugit.</p>
+        </div>
+    );
+}
+
+export default About;
